f2adef1cf74040325be023e83e3112c94e1b4449
dks07 <dilip.kumar@zingmobile.com.sg>
2023-12-19 18:52:42 +0530

    list page update for condition

--- src/components/IssueListing/IssueBody.jsx
+++ src/components/IssueListing/IssueBody.jsx
@@ -1,35 +1,140 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { routePath } from '../../constants/routes'
-import CustomModal from '../shared/common/CustomModal'
-import { Box, IconButton, Typography } from '@mui/material'
-import CloseIcon from '@mui/icons-material/Close'
-import CreateReportContent from './createReport/CreateReportContent'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { routePath } from '../../constants/routes';
+import CustomModal from '../shared/common/CustomModal';
+import { Box, IconButton, Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import CreateReportContent from './createReport/CreateReportContent';
 import Button from '@mui/material/Button';
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import { styled } from '@mui/material/styles';
 import InfoIcon from '@mui/icons-material/Info';
-
+import DownloadIcon from '@mui/icons-material/CloudDownload';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import ErrorIcon from '@mui/icons-material/Error';
 
 const IssueBody = ({ issue, index }) => {
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
-  const handleShowModal = () => setShowModal(true)
-  const handleHideModal = () => setShowModal(false)
- 
-  const HtmlTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: '#3D1B63',
-      color: '#fff',
-      maxWidth: 270,
-      minWidth: 270,
-    },
-    [`& .${tooltipClasses.arrow}`]: {
-      color: theme.palette.common.black,
-    },
-  }));
+  const handleShowModal = () => setShowModal(true);
+  const handleHideModal = () => setShowModal(false);
+
+  // ** Start: Action button/column */
+
+  const renderActionColumn = () => {
+    switch (issue.proect_status) {
+      case '0': // Not Created
+        return (
+          <td className="action-col">
+            <button className="btn report-status not-created" onClick={handleShowModal}>
+              + Create Report
+            </button>
+            <CustomModal open={showModal}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  padding: 0.5,
+                  paddingLeft: 2,
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  background: '#503998',
+                }}
+              >
+                <Typography variant="h6" sx={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>
+                  Creating document for
+                </Typography>
+                <IconButton onClick={handleHideModal}>
+                  <CloseIcon sx={{ color: 'white' }} />
+                </IconButton>
+              </Box>
+              <Box sx={{ padding: 2 }}>
+                <CreateReportContent issue={issue} />
+              </Box>
+            </CustomModal>
+          </td>
+        );
+
+      case '1': // Creating Report
+        return (
+          <td className="action-col">
+            <button className="btn report-status in-process">In Process</button>
+          </td>
+        );
+
+      case '2': // Created
+        return (
+          <td className="action-col">
+            <IconButton onClick={handleDownload}>
+              <DownloadIcon />
+            </IconButton>
+            <IconButton onClick={handleRefresh}>
+              <RefreshIcon />
+            </IconButton>
+          </td>
+        );
+
+      case '3': // Creation Error
+        return (
+          <td className="action-col">
+            <IconButton onClick={handleRefresh}>
+              <RefreshIcon />
+            </IconButton>
+            <IconButton onClick={handleError}>
+              <ErrorIcon />
+            </IconButton>
+          </td>
+        );
+
+      default:
+        return null;
+    }
+  };
+
+  const handleDownload = () => {
+    // Handle download action
+  };
+
+  const handleRefresh = () => {
+    // Handle refresh action
+  };
+
+  const handleError = () => {
+    // Handle error action
+  };
+
+  // ** End: Action button/column */
+
+  // Function to get the report status text based on proect_status value
+  const getReportStatusText = (proectStatus) => {
+    switch (proectStatus) {
+      case '0':
+        return 'Not Created';
+      case '1':
+        return 'Creating Report';
+      case '2':
+        return 'Created ';
+      case '3':
+        return 'Creation Error';
+      default:
+        return 'Unknown Status';
+    }
+  };
+
+  const reportStatusText = getReportStatusText(issue.proect_status);
+
+  const HtmlTooltip = styled(({ className, ...props }) => <Tooltip {...props} classes={{ popper: className }} />)(
+    ({ theme }) => ({
+      [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: '#3D1B63',
+        color: '#fff',
+        maxWidth: 270,
+        minWidth: 270,
+      },
+      [`& .${tooltipClasses.arrow}`]: {
+        color: theme.palette.common.black,
+      },
+    })
+  );
 
   return (
     <>
@@ -38,53 +143,71 @@ const IssueBody = ({ issue, index }) => {
         <td className="jira-col">
           <Link to={`${routePath.ISSUEBYID}${issue.jira_id}`}>{issue.jira_id}</Link>
           {/* tooltip for showing sub task */}
-          <HtmlTooltip placement="right-start" arrow title={
-              <React.Fragment>
-                <Typography color="inherit">Sub Task ID: FI2355125</Typography>
-                <div className='custom-tooltip-html contentonly'>
-                  <p>Title</p>
-                  <p className='value'>Create report for CME1</p>
-                </div>
-                <div className='custom-tooltip-html contentonly'>
-                  <p>Description</p>
-                  <p className='value'>This icon indicates that this is a sub-task assigned to you.</p>
-                </div>
-              </React.Fragment>
-            }>
-            <img src='../src/assets/Images/sub-task.png' className='subtask-icon'/>
-          </HtmlTooltip>
+          {issue.subtasks_list && issue.subtasks_list.length > 0 && (
+            <HtmlTooltip
+              placement="right-start"
+              arrow
+              title={
+                <React.Fragment>
+                  {issue.subtasks_list.map((subtask, subtaskIndex) => (
+                    <div key={subtaskIndex} className="custom-tooltip-html contentonly">
+                      <p>
+                        <Typography color="inherit">Task ID</Typography>
+                        {subtask.key}
+                      </p>
+                      <p className="value">
+                        <Typography color="inherit">Title</Typography>
+                        {subtask.summary}
+                      </p>
+                    </div>
+                  ))}
+                </React.Fragment>
+              }
+            >
+              <img src="../src/assets/Images/sub-task.png" className="subtask-icon" alt="subtask" />
+            </HtmlTooltip>
+          )}
           {/* tooltip for showing sub task end*/}
         </td>
         <td className="customer-col fw-medium">
           {issue.customer_name}
-          <HtmlTooltip placement="right-start" arrow title={
-            <React.Fragment>
-              {/* <Typography color="inherit">Customer Info</Typography> */}
-              <div className='custom-tooltip-html'>
-                <p>Customer ID</p>
-                <p className='value'>1200006</p>
-              </div>
-              <div className='custom-tooltip-html'>
-                <p>Project ID</p>
-                <p className='value'>APX100006</p>
-              </div>
-            </React.Fragment>
-            }>
-            <InfoIcon fontSize='extra-small main-color-fill' className='ml-10'/>
-          </HtmlTooltip>
-          {/* tooltip for showing sub task */}
-            <HtmlTooltip placement="right-start" arrow title={
+          <HtmlTooltip
+            placement="right-start"
+            arrow
+            title={
               <React.Fragment>
-                <div className='custom-tooltip-html contentonly'>
-                  <p>Partners</p>
-                  <p className='value'>This icon indicates that this report is created for a partner.</p>
+                <div className="custom-tooltip-html">
+                  <p>Customer ID</p>
+                  <p className="value">{issue.customer_id}</p>
+                </div>
+                <div className="custom-tooltip-html">
+                  <p>Project ID</p>
+                  <p className="value">{issue.project_id}</p>
                 </div>
               </React.Fragment>
-            }>
-            <img src='../src/assets/Images/patner-icon.png' className='subtask-icon ml-10'/>
+            }
+          >
+            <InfoIcon fontSize="extra-small" className="ml-10 main-color-fill" />
           </HtmlTooltip>
+          {/* tooltip for showing sub task */}
+          {issue.partner === '1' && (
+            <HtmlTooltip
+              placement="right-start"
+              arrow
+              title={
+                <React.Fragment>
+                  <div className="custom-tooltip-html">
+                    <p>Partners</p>
+                    <p className="value">This icon indicates that this report is created for a partner.</p>
+                  </div>
+                </React.Fragment>
+              }
+            >
+              <img src="../src/assets/Images/patner-icon.png" className="subtask-icon ml-10" alt="partner" />
+            </HtmlTooltip>
+          )}
           {/* tooltip for showing sub task end*/}
-          </td>
+        </td>
         <td className="menu-id-col">{issue.menu_id}</td>
         <td className="menu-des-col">{issue.menu_desc}</td>
         <td className="ticket-des-col">{issue.ticket_desc}</td>
@@ -94,29 +217,12 @@ const IssueBody = ({ issue, index }) => {
         <td className="assigned-col">{issue.assign_date}</td>
         <td className="created-col">{issue.created_date}</td>
         <td className="report-col">
-          <span className="report-status not-created">Not Created</span>
+          <span className={`report-status status-${issue.proect_status}`}>{reportStatusText}</span>
         </td>
-        <td className="action-col">
-          <button className="btn report-status not-created" onClick={handleShowModal}>
-            + Create Report
-          </button>
-          <CustomModal open={showModal}>
-            <Box sx={{ display: 'flex', padding: 0.5, paddingLeft: 2, justifyContent: 'space-between', alignItems: 'center', background: '#503998' }}>
-              <Typography variant="h6" sx={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>
-                Creating document for
-              </Typography>
-              <IconButton onClick={handleHideModal}>
-                <CloseIcon sx={{ color: 'white' }} />
-              </IconButton>
-            </Box>
-            <Box sx={{ padding: 2 }}>
-              <CreateReportContent issue={issue} />
-            </Box>
-          </CustomModal>
-        </td>
+        <td className="action-col">{renderActionColumn()}</td>
       </tr>
     </>
-  )
-}
+  );
+};
 
-export default IssueBody
+export default IssueBody;
