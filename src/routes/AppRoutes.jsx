b0529600c310abd827481ec13e1deab86e8c84f5
ranjeetkumar47 <rksingh6375@gmail.com>
2024-01-07 17:48:32 +0530

    Report added

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/home/Home'
 import { routePath } from '../constants/routes'
 import IssueDetailsPage from '../components/IssueListing/IssueDetailsPage'
 import Auth from '../pages/auth/Auth'
 import PageNotFound from '../pages/404/PageNotFound'
 import Dashboard from '../pages/dashboard/Dashboard'
 import AppLayout from '../layout/AppLayout'
+import Report from '../pages/report/Report'
 
 const AppRoutes = () => {
   return (
@@ -14,7 +14,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/auth" element={<Auth />} />
         <Route element={<AppLayout />}>
-          <Route path={routePath.ISSUE_LISTING} element={<Home />} />
+          <Route path={routePath.ISSUE_LISTING} element={<Report />} />
           <Route path="/" element={<Dashboard />} />
           <Route path="/issue-details/:id" element={<IssueDetailsPage />} />
           <Route path="*" element={<PageNotFound />} />
